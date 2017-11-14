4c5e8fdd346f1325dbe372f738164b4f3a278025
olegik1719 <32205310+olegik1719@users.noreply.github.com>
2017-11-14 11:41:44 +0300

    Update Example.bat

--- backup/Example.bat
+++ backup/Example.bat
@@ -6,52 +6,30 @@
 
 @set inifile=&
 
-@if "%DATE:~0,2%"=="01"(
+@if "%DATE:~0,2%"=="01" (
 	goto monthly
-) else (
-	@set /a DoW= (10%DATE:~0,2% %% 100 + ((%DATE:~6,4%) - ((14 - (10%DATE:~3,2% %% 100)) / 12)) + ((%DATE:~6,4%)^
-		- ((14 - (10%DATE:~3,2% %% 100)) / 12)) / 4 - (%DATE:~6,4% - ((14 - 10%DATE:~3,2% %% 100) / 12)) / 100 ^
-		+ (%DATE:~6,4% - ((14 - 10%DATE:~3,2% %% 100) / 12)) / 400 + 31 * (10%DATE:~3,2% %% 100 ^
-		+ 12 * ((14 - 10%DATE:~3,2% %% 100) / 12) - 2) /12 ) %% 7
-
-	@if "%DoW%"==0 (
-		goto weekly
-	)
-)
-
+) 
 @set /a DoW= (10%DATE:~0,2% %% 100 + ((%DATE:~6,4%) - ((14 - (10%DATE:~3,2% %% 100)) / 12)) + ((%DATE:~6,4%)^
 		- ((14 - (10%DATE:~3,2% %% 100)) / 12)) / 4 - (%DATE:~6,4% - ((14 - 10%DATE:~3,2% %% 100) / 12)) / 100 ^
 		+ (%DATE:~6,4% - ((14 - 10%DATE:~3,2% %% 100) / 12)) / 400 + 31 * (10%DATE:~3,2% %% 100 ^
 		+ 12 * ((14 - 10%DATE:~3,2% %% 100) / 12) - 2) /12 ) %% 7
 
-@goto day%DoW%
+@if "%DoW%"=="0" (
+		goto weekly
+	)
 
-:day0
-@echo Sunday
+
+%~dp0\backup.bat daily.ini
+goto finish
+
+:monthly 
+%~dp0\backup.bat montly.ini
+goto finish
+
+:weekly
+%~dp0\backup.bat weekly.ini
 @goto finish
 
-:day1
-@echo Monday
-@goto finish
 
-:day2
-@echo Tuesday
-@goto finish
-
-:day3
-@echo Wednesday
-@goto finish
-
-:day4
-@echo Thursday
-@goto finish
-
-:day5
-@echo Friday
-@goto finish
-
-:day0
-@echo Saturday
-@goto finish
 
 :finish
